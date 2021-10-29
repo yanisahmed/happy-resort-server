df651c2c87dadf8931845706231636cb7b19fde2
yanisahmed <yanisahmed12@gmail.com>
2021-10-29 14:38:40 +0600

    env configurration added

--- index.js
+++ index.js
@@ -1,6 +1,32 @@
 const express = require('express')
 const app = express()
+require('dotenv').config()
+const { MongoClient } = require('mongodb');
+const cors = require('cors');
+const ObjectId = require('mongodb').ObjectId;
+
 const port = process.env.PORT || 5000
+
+
+app.use(cors());
+app.use(express.json())// alternative of body parser
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.74aai.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+async function run() {
+    try {
+        await client.connect();
+        const database = client.db("happy-resort-101");
+        const usersCollection = database.collection("rooms");
+
+        console.log('database connected');
+    }
+    finally {
+        // await client.close()
+    }
+}
+run().catch(console.dir);
 
 app.get('/', (req, res) => {
     res.send('Server is running');

e790d9a0aa626cc5979265dbd618df4ecd8002ed
yanisahmed <yanisahmed12@gmail.com>
2021-10-31 16:49:03 +0600

    Delete API and readme file updated

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-# Happy Resort - Resort Booking Website
+# Happy Resort - Resort Bokking
+
+Live Link [Happy Resort](https://happy-resort-101.web.app/).
+
+## Development Tools
+- *React*
+- *React Router*
+- *Tailwind*
+- *MongoDB*
+- *NodeJS*
+- *Express*
+
+## Website Features
+- *Responsive Design*
+- *Booking System*
+- *Resort Hotel Overview*
--- index.js
+++ index.js
@@ -74,6 +74,15 @@ async function run() {
             console.log(`A document was inserted with the _id: ${result.insertedId}`);
             res.json(result);
         })
+        // DELETE API
+        app.delete('/orders/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+
+            const result = await ordersCollection.deleteOne(query);
+            console.log(result);
+            res.send(result)
+        })
     }
     finally {
         // await client.close()
